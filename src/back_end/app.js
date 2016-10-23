010ef3c3d8796e4a1dd2c2033cdae05e4a55433f
Khmara Andriy <Khmara Andriy>
2016-10-23 14:05:15 +0300

    rebuld app - app proto

--- src/back_end/app.js
+++ src/back_end/app.js
@@ -4,13 +4,14 @@ var request = require('request');
 var Logger = require('./services/logger.js');
 var MongoClient = require('mongodb').MongoClient;
 
-var url = 'http://api.openweathermap.org/data/2.5/weather?q=Rivne,ua&APPID=e9493cd95948c95df16a463acf42dac9';
+var cities = ['Rivne,ua', 'Kiev, UA', 'Lviv, UA'];
+
+
 var urlDB = 'mongodb://localhost:27017/myproject';
 
 var logger = new Logger('./logs/log.txt', false);
 
-setInterval(function () {
-
+var getWeather = function (url) {
     request(url, function (error, response, body) {
 
         if (error) {
@@ -25,11 +26,17 @@ setInterval(function () {
 
 
                 var collection = db.collection('weather');
-                // Insert some documents
-                console.log(JSON.parse(body).main);
-                console.log(collection);
-                collection.insertOne(JSON.parse(body).main, function (error, result) {
+
+
+
+                collection.insertOne({
+                    data: JSON.parse(body).main,
+                    time: JSON.parse(body).dt,
+                    city: JSON.parse(body).name,
+                    weather: JSON.parse(body).weather
+                }, function (error, result) {
                     if (error) {
+                        console.log(error);
                         logger.logError(error);
                     }
                     console.log(result);
@@ -41,5 +48,17 @@ setInterval(function () {
             });
         }
     });
+};
 
-}, 60000);
+setInterval(function () {
+
+    cities.forEach(function (city) {
+        var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=e9493cd95948c95df16a463acf42dac9';
+        getWeather(url);
+    });
+
+
+
+}, 7200000);
+
+
